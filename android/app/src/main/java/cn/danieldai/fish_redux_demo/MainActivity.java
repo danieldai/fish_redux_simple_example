6490c4ed622ab7f1108762ffd41312ed98b48f44
Daniel Dai <daeming@gmail.com>
2019-03-11 11:31:44 +0800

    Initial commit

---
+++ android/app/src/main/java/cn/danieldai/fish_redux_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.danieldai.fish_redux_demo;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
